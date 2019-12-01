bfc51b60e0bb93c652ceb3108d8ed156f70cd343
hee20192 <html20192@daum.net>
2019-12-01 11:53:41 +0900

    multer.storage

--- routes/multer.js
+++ routes/multer.js
@@ -2,7 +2,30 @@ var express = require('express');
 var router = express.Router();
 var multer  = require('multer');
 var path = require('path');
+var fs = require('fs');
 var upload = multer({ dest: path.join(__dirname, '../public/uploads/') });
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, getPath());
+  },
+  filename: function (req, file, cb) {
+    var saveFile = getFile(file.originalname); //{} -> originalname -> 1912-12333333-99.jpg
+    cb(null, saveFile.saveName);
+  }
+});
+function getPath() {
+  var newPath = path.join(__dirname, "../public/uploads/" + makePath());
+  if(!fs.existsSync(newPath)) {
+    fs.mkdirSync(newPath);
+  }
+  return newPath; //1912
+}
+function makePath() {
+  var d = new Date();
+  var year = d.getFullYear().substr(2); //19
+  var month = (d.getMonth() + 1 < 10) ? "0" + (d.getMonth() + 1) : d.getMonth() + 1;
+  return year + month;
+}
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
