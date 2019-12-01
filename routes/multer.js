2f1a2947c428c6fecb3ef51f0a6e525b7917ed31
hee20192 <html20192@daum.net>
2019-12-01 12:18:45 +0900

    multer.js 작업중

--- routes/multer.js
+++ routes/multer.js
@@ -3,28 +3,41 @@ var router = express.Router();
 var multer  = require('multer');
 var path = require('path');
 var fs = require('fs');
-var upload = multer({ dest: path.join(__dirname, '../public/uploads/') });
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, getPath());
   },
   filename: function (req, file, cb) {
     var saveFile = getFile(file.originalname); //{} -> originalname -> 1912-12333333-99.jpg
-    cb(null, saveFile.saveName);
+    console.log(saveFile.filename);
+    cb(null, saveFile.filename);
   }
 });
+var upload = multer({ storage: storage });
 function getPath() {
   var newPath = path.join(__dirname, "../public/uploads/" + makePath());
   if(!fs.existsSync(newPath)) {
     fs.mkdirSync(newPath);
   }
-  return newPath; //1912
+  return newPath;
 }
 function makePath() {
   var d = new Date();
-  var year = d.getFullYear().substr(2); //19
+  var year = String(d.getFullYear()).substr(2); //19
   var month = (d.getMonth() + 1 < 10) ? "0" + (d.getMonth() + 1) : d.getMonth() + 1;
   return year + month;
+}
+function getFile(oriFile) {
+  // oriFile = "sample.jpg"
+  var ext = path.extname(oriFile);  //.jpg
+  var fname = path.basename(oriFile, ext); //sample
+  var f1 = makePath();  //1912
+  var f2 = Date.now(); //timestamp
+  var f3 = Math.floor(Math.random() * 90 + 10);
+  var filename = f1 + '-' + f2 + '-' + f3 + ext;
+  console.log(filename);
+
+  return {oriFile, ext, fname, filename};
 }
 
 /* GET home page. */
