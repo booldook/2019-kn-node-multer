757f23f5390360c7816d6ca9bbd05a4e496aa8fb
hee20192 <html20192@daum.net>
2019-12-01 11:36:13 +0900

    multer

--- routes/multer.js
+++ routes/multer.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer');
+var path = require('path');
+var upload = multer({ dest: path.join(__dirname, '../public/uploads/') });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('multer-list', { title: 'Express' });
+});
+
+router.post('/', upload.single('img'), (req, res, next) => {
+  res.send("저장되었습니다."+req.file.filename);
 });
 
 module.exports = router;
